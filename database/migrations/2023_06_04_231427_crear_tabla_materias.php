30084afa0a9c132c950a81da645a64ed585f4a27
sharol2929 <sharol3222@gmail.com>
2023-06-04 21:35:08 -0500

    Initial commit

---
+++ database/migrations/2023_06_04_231427_crear_tabla_materias.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materias', function (Blueprint $table) {
+            $table->char('codmateria',2); 
+            $table->primary('codmateria');
+            $table->char('nommateria',20);
+            $table->integer('cremateria');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materias');
+    }
+};
---
+++ database/migrations/2023_06_04_231957_crear_tabla_barrios.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barrios', function (Blueprint $table) {
+            $table->char('codbarrio',3); 
+            $table->primary('codbarrio');
+            $table->char('nombarrio',30);
+            $table->integer('estbarrio');
+            $table->integer('comuna');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barrios');
+    }
+};
